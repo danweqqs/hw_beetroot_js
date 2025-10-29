e0aa2783fada8b9719fdd7f816039e946f6e509d
danweqqs <daniilsorokin381@gmail.com>
2025-10-29 12:08:37 +0200

    add playlist with button

--- js/main.js
+++ js/main.js
@@ -1,112 +1,52 @@
 'use strict';
 
-
-let shoppingList = [
-    { name: 'iPhone 17', quantity: 1, isBought: false, price: 45000, total: 45000 },
-    { name: 'MacBook Air M3', quantity: 1, isBought: true, price: 58000, total: 58000 },
-    { name: 'AirPods Pro 2', quantity: 2, isBought: false, price: 12000, total: 24000 },
-    { name: 'Apple Watch SE', quantity: 1, isBought: true, price: 12500, total: 12500 }
+var playList = [
+    {
+        author: "LED ZEPPELIN",
+        song: "STAIRWAY TO HEAVEN"
+    },
+    {
+        author: "QUEEN",
+        song: "BOHEMIAN RHAPSODY"
+    },
+    {
+        author: "LYNYRD SKYNYRD",
+        song: "FREE BIRD"
+    },
+    {
+        author: "DEEP PURPLE",
+        song: "SMOKE ON THE WATER"
+    },
+    {
+        author: "JIMI HENDRIX",
+        song: "ALL ALONG THE WATCHTOWER"
+    },
+    {
+        author: "AC/DC",
+        song: "BACK IN BLACK"
+    },
+    {
+        author: "QUEEN",
+        song: "WE WILL ROCK YOU"
+    },
+    {
+        author: "METALLICA",
+        song: "ENTER SANDMAN"
+    }
 ];
 
-// 1 вивести весь список покупок
-function showList() {
-    console.log('Список покупок:');
-
-    console.log('Не куплено: ');
-    for (let item of shoppingList) {
-        if (!item.isBought) {
-            console.log(`${item.name} — ${item.quantity} шт × ${item.price} грн = ${item.total} грн`);
-        }
-    }
-    console.log('Куплено: ');
-    for (let item of shoppingList) {
-        if (item.isBought) {
-            console.log(`${item.name} — ${item.quantity} шт × ${item.price} грн = ${item.total} грн`);
-        }
-    }
+var list = document.getElementById("playlist");
+for (var i = 0; i < playList.length; i++) {
+    var item = document.createElement("li");
+    item.textContent = playList[i].author + " — " + playList[i].song;
+    list.appendChild(item);
 }
 
-
-// 2 позначити товар як куплений
-function buyProduct(productName) {
-    const product = shoppingList.find(item => item.name.toLowerCase() === productName.toLowerCase());
-
-    if (product) {
-        product.isBought = true;
-        console.log(`Товар "${product.name}" позначено як куплений.`);
-    } else {
-        console.log(`Товар "${productName}" не знайдено у списку.`);
-    }
-}
-
-
-// 3 видалити товар зі списку
-function deleteProduct(productName) {
-    const newList = shoppingList.filter(item => item.name.toLowerCase() !== productName.toLowerCase());
-
-    if (newList.length === shoppingList.length) {
-        console.log(`Товар "${productName}" не знайдено у списку.`);
-    } else {
-        shoppingList = newList;
-        console.log(`Товар "${productName}" видалено зі списку.`);
-    }
-}
-
-
-// 4 додати товар у список або оновити кількість, якщо він уже є
-function addProduct(name, quantity, price) {
-    const existingProduct = shoppingList.find(item => item.name.toLowerCase() === name.toLowerCase());
-
-    if (existingProduct) {
-        existingProduct.quantity += quantity;
-        existingProduct.total = existingProduct.quantity * existingProduct.price;
-        console.log(`Оновлено кількість: ${existingProduct.name} (${existingProduct.quantity} шт, ${existingProduct.total} грн).`);
-    } else {
-        shoppingList.push({
-            name,
-            quantity,
-            price,
-            total: quantity * price,
-            isBought: false
-        });
-        console.log(`Додано новий товар: ${name}.`);
-    }
-}
-
-
-// 5 підрахунок загальної суми всіх товарів
-function calcTotalSum() {
-    let total = 0;
-    for (let item of shoppingList) {
-        total += item.total;
-    }
-    console.log(`Загальна сума покупок: ${total} грн.`);
-    return total;
-}
-
-
-// 6 підрахунок суми куплених або некуплених товарів
-function calcSumByStatus(isBought) {
-    let total = 0;
-
-    for (let item of shoppingList) {
-        if (item.isBought === isBought) {
-            total += item.total;
-        }
-    }
-    const statusText = isBought ? 'куплених' : 'ще не куплених';
-    console.log(`Сума ${statusText} товарів: ${total} грн.`);
-    return total;
-}
-
-showList();
-buyProduct('AirPods Pro 2');
-addProduct('iPad Air', 1, 18000);
-addProduct('iPhone 17', 1, 45000);
-deleteProduct('Apple Watch SE');
-
-showList();
-
-calcTotalSum();
-calcSumByStatus(true);
-calcSumByStatus(false);
+var buttonPlace = document.getElementById("buttonPlace");
+var button = document.createElement("button");
+button.textContent = "YouTube";
+button.classList.add("video-button");
+button.onclick = function() {
+  window.location.href = "https://www.youtube.com/watch?v=dnyrEoCJP_w";
+};
+buttonPlace.appendChild(button);
